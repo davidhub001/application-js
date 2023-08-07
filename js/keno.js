eb93a4037b67c8587e137cf37d7bd200703a3d4b
david.propitech <david.propitech@gmail.com>
2023-08-07 13:22:35 +0300

    main

---
+++ js/keno.js
@@ -0,0 +1,146 @@
+
+var generate = document.getElementById("generate");
+
+// Créer les boutons pour l'en-tête
+for (let i = 1; i <= 8; i++) {
+    let button = document.createElement("button");
+    button.textContent = i;
+    button.setAttribute("class","button_select");
+    document.getElementById("headerRow").appendChild(document.createElement("td").appendChild(button));
+}
+var button_select = document.querySelectorAll(".button_select");
+button_select.forEach(button => {
+    button.addEventListener("click", function() {
+        button_select.forEach(b=>{
+            b.classList.remove('none');
+            b.removeAttribute('value');
+            b.removeAttribute("selected_number")
+
+        })
+        var data_table=document.querySelectorAll(".data_table");
+        data_table.forEach(data=>{
+            data.classList.remove('none');
+            data.classList.remove('ok');
+            data.removeAttribute('disabled');
+            data.removeAttribute('val_selected');
+        })
+        
+        generate.removeAttribute("class");
+        
+
+        this.classList.add('none');
+        this.setAttribute("value",this.textContent)
+        this.setAttribute("selected_number",'on')
+        setselected_number(this.textContent);
+    });
+});
+
+// Remplir le tableau de données
+let dataTable = document.getElementById("dataTable");
+let value = 1;
+for (let k = 1; k <= 8; k++) {
+    let row = document.createElement("tr");
+    for (let i = 1; i <= 10; i++) {
+        let cell = document.createElement("td");
+        let button = document.createElement("button");
+        button.textContent = value;
+        button.setAttribute("value",value)
+        button.setAttribute("class","data_table")
+        button.setAttribute("id",value)
+        cell.appendChild(button);
+        row.appendChild(cell);
+        value++;
+    }
+    dataTable.appendChild(row);
+}
+function setselected_number(num){
+    var nb_click = 0;
+    var data_table=document.querySelectorAll(".data_table");
+    data_table.forEach(data => {
+        data.addEventListener('click',function(){
+            if(nb_click < num){
+                this.setAttribute("disabled","true");
+                this.setAttribute("val_selected","true");
+                generate.setAttribute("class","none");
+
+                this.classList.add("none");
+                nb_click++;
+            }
+        })
+    })
+
+}
+
+
+// Utilisation de la fonction pour générer 5 tirages de Keno, avec des nombres de 1 à 80 et 10 numéros sélectionnés pour chaque tirage
+const numDraws = 1;
+const minNumber = 1;
+const maxNumber = 80;
+const numSelected = 20;
+
+
+generate.onclick = function(){
+    var kenoDraws = generateKenoNumbers(numDraws, minNumber, maxNumber, numSelected);
+
+    var arrayliste_element = [];
+    var data_element = document.querySelectorAll("[val_selected=true]");
+    data_element.forEach(function(chiffre){
+        arrayliste_element.push(chiffre.textContent);
+        chiffre.classList.remove('none');
+        chiffre.removeAttribute('disabled');
+    })
+    var mise =document.getElementById("mise");
+    mise.textContent = arrayliste_element.join(",");
+
+        const delay = 1000; // Délai entre chaque chiffre (en millisecondes)
+        var numbers = kenoDraws[0]
+        shuffleArray(numbers);
+        const typewriterDiv = document.getElementById('valeur');
+
+        let numberIndex = 0;
+
+        function typeWriter() {
+            if (numberIndex < numbers.length) {
+
+                var buttonval = document.getElementById(numbers[numberIndex]);
+                buttonval.classList.add("ok");
+
+                typewriterDiv.textContent = numbers[numberIndex];
+                numberIndex++;
+                setTimeout(typeWriter, delay);
+            }
+        }
+
+        typeWriter();
+
+}
+////ici/////
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+function generateKenoNumbers(numDraws, minNumber, maxNumber, numSelected) {
+    // Vérifier les valeurs valides
+    if (numDraws <= 0 || numSelected <= 0 || minNumber >= maxNumber) {
+        return [];
+    }
+    
+    // Générer les numéros pour chaque tirage
+    const kenoResults = [];
+    for (let i = 0; i < numDraws; i++) {
+        const drawnNumbers = [];
+        while (drawnNumbers.length < numSelected) {
+            const randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+            if (!drawnNumbers.includes(randomNumber)) {
+                drawnNumbers.push(randomNumber);
+            }
+        }
+        drawnNumbers.sort((a, b) => a - b);
+        kenoResults.push(drawnNumbers);
+    }
+    
+    return kenoResults;
+}
+
